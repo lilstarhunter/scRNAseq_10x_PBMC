55cc759b13a08364496c479a419b395d539ccabb
lilstarhunter <lmstein22@gmail.com>
2021-07-07 11:44:50 -0400

    documented script

--- scripts/load_create_merge/single_seurat_metadata_save.R
+++ scripts/load_create_merge/single_seurat_metadata_save.R
@@ -1,10 +1,21 @@
-library(Seurat)
-library(dplyr)
-library(Matrix)
+# SCRIPT FUNCITON: read a 10x dataset, create a seurat object, and add metadata 
+# OUTPUT: RDS file containing new metadata and a seurat object
+
+
+# Dataset Specifications
+# 1. Single dataset, single batch (no merging required)
+# 2. TAR of RAW data with barcodes.tsv, matrix.mtx and features.tsv
+# 3. No biological/experimental groups added at this point
+# 4. Human Query
+# *** Note: RDS files at this step will all be named with the suffix seraut_meta_v1.rds *** #
 
 # ******************************************************* #
 # *********** ALL FILES / PATHS ON PMACS HPC ************ #
 # ******************************************************* #
+
+library(Seurat)
+library(dplyr)
+library(Matrix)
 
 
 options(warn = -1) #Warning about underscored features converted to dashes ignored
@@ -15,7 +26,7 @@ options(warn = -1) #Warning about underscored features converted to dashes ignor
 df.data <- Read10X(data.dir = "/home/steinlm/scRNAseq_10x_PBMC/data/filtered_gene_bc_matrices/hg19/")
 
 # Initialize the Seurat object with the raw (non-normalized data).
-df <- CreateSeuratObject(counts = df.data, project = "name", min.cells = 3, min.features = 200)
+df <- CreateSeuratObject(counts = df.data, project = "name", min.cells = 3, min.features = 200) #Make sure to change name of project to something relevant
 
 # ============================ #
 # === Create MetaData ======== #
@@ -38,7 +49,7 @@ df$mitoRatio <- df@meta.data$percent.mt / 100
 # ================================ #
 # ===== Save RDS/MetaData ======== #
 # ================================ #
-saveRDS(df, file="/home/steinlm/scRNAseq_10x_PBMC/data/initialfile_meta.rds")
+saveRDS(df, file="/home/steinlm/scRNAseq_10x_PBMC/data/pbmc_ser_meta_v1.rds") #Rename
 
 
 
